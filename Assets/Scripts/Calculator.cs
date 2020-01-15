12750a8600a7dcbcc40a59fcbb3a46f4a0bf6813
unknown <eva.schwarzl@htl-salzburg.ac.at>
2020-01-15 10:54:07 +0100

    finished Calculator (Add, Sub, Mult, Div)

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+
+    public Text Result;
+    public Text Operation;
+    public InputField ipfnumber1;
+    public InputField ipfnumber2;
+    public Button add;
+
+    public float varA;
+    public float varB;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Addition ()
+    {
+
+        varA = float.Parse(ipfnumber1.text);
+        varB = float.Parse(ipfnumber2.text);
+
+        float varAdd = varA + varB;
+
+        Operation.text = "+";
+        Result.text = varAdd.ToString();
+    }
+
+    public void Subtraction()
+    {
+
+        varA = float.Parse(ipfnumber1.text);
+        varB = float.Parse(ipfnumber2.text);
+
+        float varSub = varA - varB;
+
+        Debug.Log("Result= " + varSub.ToString());
+
+        Operation.text = "-";
+        Result.text = varSub.ToString();
+    }
+
+    public void Multiplication()
+    {
+
+        varA = float.Parse(ipfnumber1.text);
+        varB = float.Parse(ipfnumber2.text);
+
+        float varMul = varA * varB;
+
+        Debug.Log("Result= " + varMul.ToString());
+
+        Operation.text = "times";
+        Result.text = varMul.ToString();
+    }
+
+    public void Division()
+    {
+
+        varA = float.Parse(ipfnumber1.text);
+        varB = float.Parse(ipfnumber2.text);
+
+        float varDiv = varA / varB;
+
+        Debug.Log("Result= " + varDiv.ToString());
+
+        Operation.text = "by";
+        Result.text = varDiv.ToString();
+    }
+
+
+}
